b0f65dc578eac321af34fd92135bdeeadcca6690
Chirag Soni <csoni0693@gmail.com>
2025-01-18 07:53:12 +0530

    atm-system

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
