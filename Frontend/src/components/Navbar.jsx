0c302922171c74aca6fc953108a924b9d3161060
Chirag Soni <csoni0693@gmail.com>
2025-01-18 08:34:45 +0530

    atm-system

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 import { CreditCard, LogOut, Settings, User } from "lucide-react";
+import { UserData } from "../context/UserContext";
 
 const Navbar = () => {
-
-
+  const {logout} = UserData()
   return (
     <header
       className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 
@@ -31,6 +31,11 @@ const Navbar = () => {
               <Settings className="w-4 h-4" />
               <span className="hidden sm:inline">Settings</span>
             </Link>
+
+            <button className="flex gap-2 items-center" onClick={logout}>
+                  <LogOut className="size-5" />
+                  <span className="hidden sm:inline">Logout</span>
+                </button>
 
             {/* {authUser && (
               <>
